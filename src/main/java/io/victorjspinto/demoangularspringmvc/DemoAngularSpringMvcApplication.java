53f3c912953b8e7c2882a79709454a31adecbeac
Victor Pinto <victor.pinto@olxbr.com>
2018-02-23 19:08:40 -0300

    Spring boot initializr generated project

---
+++ src/main/java/io/victorjspinto/demoangularspringmvc/DemoAngularSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package io.victorjspinto.demoangularspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAngularSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAngularSpringMvcApplication.class, args);
+	}
+}
---
+++ src/test/java/io/victorjspinto/demoangularspringmvc/DemoAngularSpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package io.victorjspinto.demoangularspringmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoAngularSpringMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
